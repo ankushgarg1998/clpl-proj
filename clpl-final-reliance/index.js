3d9a381bd33d3c7f25abe956f08e534f33cfa290
ankushgarg1998 <1998anky@gmail.com>
2019-03-25 22:48:41 +0530

    progress

--- clpl-final-reliance/index.js
+++ clpl-final-reliance/index.js
@@ -1,10 +1,31 @@
 var master = {};
+
+// Cruciol
 var barcode_column = 'z';
 var date_column = 'z';
 var price_column = 'z';
 var qty_column = 'z';
+
+// essential
+var metsize_column = 'z';
+var desc_column = 'z';
+var articleno_column = 'z';
+var labeltype_column = 'z';
+
+// No Changes
+var style_column = 'z';
+var color_column = 'z';
+var size_column = 'z';
+var vendor_column = 'z';
+var fashiongradedesc_column = 'z';
+var barcolor_column = 'z';
+
+
 var collect_column = 'Collection';
 
+
+
+// FUNCTIONS
 addToMaster = function (master_key, el) {
     if (master_key in master) {
         master[master_key][qty_column] += el[qty_column];
@@ -55,6 +76,9 @@ findWord = function (word, str) {
     return str.split(' ').some(function (w) { return w.toUpperCase() === word.toUpperCase() })
 }
 
+
+
+// WORKING
 $('#input-excel').change(function (e) {
     var reader = new FileReader();
     reader.readAsArrayBuffer(e.target.files[0]);
@@ -67,7 +91,7 @@ $('#input-excel').change(function (e) {
         var sheet_json = XLSX.utils.sheet_to_json(sheet);
         console.log(sheet_json);
 
-        var el = sheet_json[0];
+        var el = sheet_json[1];
         Object.keys(el).forEach(function (key, index) {
             if (findWord('MRP', key))
                 price_column = key;
@@ -75,26 +99,89 @@ $('#input-excel').change(function (e) {
                 barcode_column = key;
             if (findWord('MFD', key) || findWord('Yrmonth', key))
                 date_column = key;
-            if (findWord('QTY', key))
+            if (findWord('LABEL', key) && findWord('QTY', key))
                 qty_column = key;
+
+            if (findWord('METSIZE', key))
+                metsize_column = key;
+            if (findWord('DESC', key))
+                desc_column = key;
+            if (findWord('ARTICLE', key))
+                articleno_column = key;
+            if (findWord('LABEL', key) && findWord('TYPE', key))
+                labeltype_column = key;
+
+            if (findWord('STYLE', key))
+                style_column = key;
+            if (findWord('COLOR', key))
+                color_column = key;
+            if (findWord('SIZE', key))
+                size_column = key;
+            if (findWord('VENDOR', key))
+                vendor_column = key;
+            if (findWord('FASHION', key) && findWord('DESC', key))
+                fashiongradedesc_column = key;
+            if (findWord('BAR', key) && findWord('COLOUR', key))
+                barcolor_column = key;
         });
 
+        
         if (barcode_column === 'z')
-            alert(`Barcode Column not found. Should have the work "EAN".`);
+            alert(`Barcode Column not found. Should have the word "EAN".`);
         if (date_column === 'z')
-            alert(`Date Column not found. Should have the word "MFD".`);
+            alert(`Date Column not found. Should have the word "MFD" or "Yrmonth".`);
         if (price_column === 'z')
             alert(`MRP column not found. Should have the word "MRP".`)
         if (qty_column === 'z')
-            alert(`Quantity column not found. Should have the word "QTY".`)
+            alert(`Quantity column not found. Should have the word "Label" and "QTY".`)
 
+        if (metsize_column === 'z')
+            alert(`Metsize column not found. Should have the word "METSIZE".`)
+        if (desc_column === 'z')
+            alert(`Desc Column not found. Should have the work "DESC".`);
+        if (articleno_column === 'z')
+            alert(`Article No Column not found. Should have the word "ARTICLE".`);
+        if (labeltype_column === 'z')
+            alert(`Label Type Column not found. Should have the word "LABEL" and "TYPE".`);
+            
+        if (style_column === 'z')
+            alert(`Style Code Column not found. Should have the word "STYLE".`);
+        if (color_column === 'z')
+            alert(`Color Column not found. Should have the word "COLOR".`);
+        if (size_column === 'z')
+            alert(`Size column not found. Should have the word "SIZE".`)
+        if (vendor_column === 'z')
+            alert(`Vendor Column not found. Should have the word "VENDOR".`);
+        if (fashiongradedesc_column === 'z')
+            alert(`Fashion Grade Description Column not found. Should have the word "FASHION" and "DESC".`);
+        if (barcolor_column === 'z')
+            alert(`Barcode Color column not found. Should have the word "BAR" and "COLOUR".`)
+
+        
         if(barcode_column === 'z' || date_column === 'z' || price_column === 'z' || qty_column === 'z')
+            return;
+        if(metsize_column==='z' || desc_column==='z' || articleno_column==='z' || labeltype_column==='z')
+            return;
+        if(style_column==='z' || color_column==='z' || size_column==='z' || vendor_column==='z' || fashiongradedesc_column==='z' || barcolor_column==='z')
             return;
 
         console.log(`barcode_column: ${barcode_column}`);
         console.log(`date_column: ${date_column}`);
         console.log(`price_column: ${price_column}`);
         console.log(`qty_column: ${qty_column}`);
+        console.log(`---------------------`);
+        console.log(`metsize_column: ${metsize_column}`);
+        console.log(`desc_column: ${desc_column}`);
+        console.log(`articleno_column: ${articleno_column}`);
+        console.log(`labeltype_column: ${labeltype_column}`);
+        console.log(`---------------------`);
+        console.log(`style_column: ${style_column}`);
+        console.log(`color_column: ${color_column}`);
+        console.log(`size_column: ${size_column}`);
+        console.log(`vendor_column: ${vendor_column}`);
+        console.log(`fashiongradedesc_column: ${fashiongradedesc_column}`);
+        console.log(`barcolor_column: ${barcolor_column}`);
+
 
         sheet_json.forEach(el => {
             // console.log(el);
@@ -108,9 +195,23 @@ $('#input-excel').change(function (e) {
 
         var new_json = [];
         for (var key in master) {
+
+            // Extra Qty and Total Quantity
+            var qtybuffer = parseInt(document.getElementById('qtybuffer').value);
+            var curval = master[key][qty_column];
+            var extraqty = Math.round((qtybuffer/100)*curval);
+            master[key]["Extra Qty"] = extraqty;
+            master[key]["Total Qty"] = curval + extraqty;
+            
             new_json.push(master[key]);
         }
 
+        // var qqq = {};
+        // qqq['cat'] = 5;
+        // qqq['bat'] = "123";
+        // qqq.abc = 1;
+        // qqq['bsd'] = "12345";
+        // var new_sheet = XLSX.utils.json_to_sheet(qqq);
         var new_sheet = XLSX.utils.json_to_sheet(new_json);
 
         var new_workbook = XLSX.utils.book_new();
